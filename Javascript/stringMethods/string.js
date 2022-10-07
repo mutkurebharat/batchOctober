83608fa084216134c919e772a58215553a11e6c1
BharatM <mutkurebharat@gmail.com>
2022-10-07 08:59:24 +0530

    String Methods

--- Javascript/stringMethods/string.js
+++ Javascript/stringMethods/string.js
@@ -42,11 +42,93 @@ var substr6 = str.substr(10, -2);
 
 var slice10 = str.slice(0, 5);
 
-console.log(substr1);
-console.log(substr2);
-console.log(substr3);
-console.log(substr4);
-console.log(substr5);
-console.log("substr6 = ", substr6);
+// console.log(substr1);
+// console.log(substr2);
+// console.log(substr3);
+// console.log(substr4);
+// console.log(substr5);
+// console.log("substr6 = ", substr6);
 
-console.log(slice10);
+// console.log(slice10);
+
+////////// replace ///////////////
+
+var str = "Working on string methods string";
+
+// var replace1 = str.replace("string", "number");
+// var replace2 = str.replace(/StrInG/i, "Number");
+// var replace3 = str.replace(/StrInG/ig, "Numberrrrrr");
+
+
+// console.log(replace1);
+// console.log(replace2);
+// console.log(replace3);
+
+/////// toUpperCase /////////////////
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+//////////// Concat /////////////////
+
+// var a = "ABC"
+// var b = "DEF"
+// var c = "GHI"
+
+// console.log(a + b + c);
+
+// var d = a.concat(" ",b, " ",c);
+// console.log(d);
+// console.log(a);
+
+
+//////////////// trim /////////////////
+
+// var trimmm = "      Good         "
+// // console.log("before = ", trimmm.length);
+// // var trim1 = trimmm.trim()
+// // console.log("after = ", trim1.length);
+// console.log("TrimStart = ", trimmm.trimStart().length);
+// console.log("TrimStart = ", trimmm.trimEnd().length);
+
+///////////// toString() ///////////////
+
+// var num = 10;
+
+// console.log(typeof num);
+
+// var toStr = num.toString();
+// console.log(typeof toStr);
+
+///////////// indexOf ///////////////////
+// if doesn't match it will return -1 
+
+var str = "string Working on string methods string";
+
+// console.log(str.length);
+
+// var index1 = str.indexOf("string");
+// var index2 = str.lastIndexOf("string")
+// var index3 = str.indexOf("string", 7)
+// console.log("First = ", index1);
+// console.log("Last = ", index2);
+// console.log("Middle = ", index3);
+
+////////////// charAt ///////////////////////
+
+// var char1 = str.charAt(10000);
+// console.log(char1);
+
+// var charcode = str.charCodeAt(10);
+// console.log(charcode);
+
+////////////// split //////////////////////
+// convert string to array
+var str = "string Working on string methods string";
+
+var split1 = str.split();
+var split2 = str.split(' ')
+var split3 = str.split('')
+// console.log(split1);
+console.log(typeof split2);
+// console.log(split3);
