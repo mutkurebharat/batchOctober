f325b28eabc71a494460556621bd544c327711c4
BharatM <mutkurebharat@gmail.com>
2022-10-11 09:07:18 +0530

    Splice, Sort

--- Javascript/arrayMethods/array.js
+++ Javascript/arrayMethods/array.js
@@ -63,3 +63,68 @@ var constructorArr = new Array(10);
 // var constructorArr1 = new Array(10,11)
 // console.log(constructorArr1);
 
+
+////////////////// delete /////////////////////
+// var arr = [1, 2, 3, "A", "B", true];
+// delete arr[2];
+
+// console.log(arr);
+// console.log(arr.length);
+
+////////// concat //////////////////
+
+// var a = [1,2,3,4,5];
+// var b = [6,7,8,9,10];
+// var c = [11,12,13,14,15];
+
+// var d = a.concat(b,c);
+// console.log(d);
+
+/////////  splice ////////////////
+// splice(startPosotion, no of elements to be deleted, ele1, ele2, ...)
+var arr = [1, 2, 3, "A", "B", true];
+
+
+// arr.splice(3,2,"X","Y","Z");
+// arr.splice(3,0,"Q","W");
+
+// arr.splice(3);
+// console.log(arr);
+
+///////// toString //////////// 
+
+// console.log(arr.toString());
+
+////////////// Sort /////////////////////////////
+
+var charArr = ["X", "A", "Z", "C", "B", "M"];
+var numArr = [5,3,9,2,55,1,23,36,48,62];
+
+// console.log(charArr.sort());
+// console.log(numArr.sort());
+// console.log(numArr.sort(compareFunc));
+
+// console.log(
+//   numArr.sort(function compareFunc(a, b) {
+//     return b - a;
+//   })
+// );
+
+function compareFunc(a,b) {
+    return a - b;
+}
+
+///////////////// Reverse ///////////////
+
+// console.log(charArr.reverse());
+// console.log(numArr.reverse());
+
+
+////// Highest or Lowest Value /////////
+
+var sortArr = numArr.sort(compareFunc);
+var highest = sortArr[sortArr.length - 1];
+lowest = sortArr[0];
+console.log(sortArr);
+console.log("Highest = ", highest);
+console.log("Lowest = ", lowest);
