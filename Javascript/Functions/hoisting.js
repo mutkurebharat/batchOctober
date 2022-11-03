6294de43f92f252b70613e0bb13fe588bdc675ba
mutkurebharat <34864760+mutkurebharat@users.noreply.github.com>
2022-11-03 09:22:19 +0530

    Create hoisting.js

---
+++ Javascript/Functions/hoisting.js
@@ -0,0 +1,23 @@
+// COde EXecution  2 steps
+// 1. Compilation - memory gets allocated to variable and function declaration 
+// 2. EXecution - EXecute the COde
+
+
+
+// console.log(a);
+// var a = "Something";
+// console.log(a);
+
+
+
+console.log(add)
+function add(a,b){
+    return a + b;
+}
+
+
+// compilation - 
+// var a = undefined;
+//add: function add(a,b){
+  //  return a + b;
+//}
