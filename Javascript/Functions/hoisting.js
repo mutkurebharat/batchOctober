1941d9979f65dcfe9d7a2ee913a8a0b0c767136b
mutkurebharat <mutkurebharat.com>
2022-11-17 09:46:18 +0530

    Hoisting

--- Javascript/Functions/hoisting.js
+++ Javascript/Functions/hoisting.js
@@ -2,22 +2,23 @@
 // 1. Compilation - memory gets allocated to variable and function declaration 
 // 2. EXecution - EXecute the COde
 
+debugger;
+// console.log("Before = ", a);
+// var a = 20;
+// console.log("After = ", a);
+
+// console.log(some());
+// function some(){
+//   return "Something"
+// }
 
 
-// console.log(a);
-// var a = "Something";
-// console.log(a);
+// console.log("Before = ", hoistingExpression())
+// var hoistingExpression = function(){
+//   return "Hoisting Expression"
+// }
 
+console.log(z);
+let z = 30;
 
-
-console.log(add)
-function add(a,b){
-    return a + b;
-}
-
-
-// compilation - 
-// var a = undefined;
-//add: function add(a,b){
-  //  return a + b;
-//}
+// TDZ (Temporal Dead Zone)
